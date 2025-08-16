8a575d767c602d42c39f5ba036d57ad453c9ee1d
adhy2312 <adhithyamohan2312@gmail.com>
2025-08-16 16:12:06 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
     tailwindcss: {},
-=======
-    "@tailwindcss/postcss": {},
->>>>>>> 83b75281ca7decdfe68d95fe4cb5e70eecb84bad
     autoprefixer: {},
   },
 };
